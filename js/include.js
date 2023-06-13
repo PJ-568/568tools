c7f7efafe578fb3209cf2cb50b2f400a4dea7ba1
PJ568 <PJ568@PJ568.eu.org>
2023-06-13 20:25:23 +0800

    优化右键菜单

--- js/include.js
+++ js/include.js
@@ -1,9 +1,10 @@
 function includeHTML() {
-    var elements = document.getElementsByTagName("include");
-    for (var i = 0; i < elements.length; i++) {
-      var element = elements[i];
-      var file = element.getAttribute("src");
-      if (file) {
+  var elements = document.getElementsByTagName("include");
+  for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+    var file = element.getAttribute("src");
+    if (file) {
+      (function(element) { // 创建一个函数作用域，以便保存每个元素的引用
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
           if (this.readyState === 4 && this.status === 200) {
@@ -12,8 +13,9 @@ function includeHTML() {
         };
         xhttp.open("GET", file, true);
         xhttp.send();
-      }
+      })(element);
     }
   }
-  
-  document.addEventListener("DOMContentLoaded", includeHTML);  
+}
+
+document.addEventListener("DOMContentLoaded", includeHTML);
