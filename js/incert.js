1c4ea2cd873d76903ea1065ebbe19eacf30e3254
PJ568 <PJ568@PJ568.eu.org>
2023-06-18 11:15:14 +0800

    添加动态标题支持

--- js/incert.js
+++ js/incert.js
@@ -8,13 +8,26 @@ function getUrlParameter(name) {
 
 // 替换 iframe 中的网页并聚焦视角
 function replaceIframeSource() {
-	var link = getUrlParameter('link');
-	if (link) {
-		var iframe = document.getElementById('myIframe');
-		iframe.src = link;
-		iframe.scrollIntoView({ behavior: 'smooth' });
-	}
+    var link = getUrlParameter('link');
+    if (link) {
+      	var iframe = document.getElementById('myIframe');
+      	iframe.src = link;
+      	iframe.scrollIntoView({ behavior: 'smooth' });
+
+      	// 监听 iframe 的加载事件
+      	iframe.addEventListener('load', function() {
+        	// 获取 iframe 内部文档的标题
+        	var iframeTitle = iframe.contentDocument.title;
+
+        	// 更新 <title> 标签的内容
+        	var dynamicTitle = document.getElementById('dynamicTitle');
+        	dynamicTitle.innerHTML = "568Tools | " + iframeTitle;
+      	});
+    }
 }
 
-// 页面加载完成时调用替换函数
-window.onload = replaceIframeSource;
+  	// 页面加载完成时调用替换函数
+window.onload = function() {
+	replaceIframeSource();
+	// 添加你的代码...
+};
