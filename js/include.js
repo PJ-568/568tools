889e7b034577f0ae249c77567b9ee1612723d96a
PJ568 <PJ568@PJ568.eu.org>
2023-09-07 17:11:45 +0800

    修复不能执行 js 的问题

--- js/include.js
+++ js/include.js
@@ -1,19 +1,45 @@
+// function includeHTML() {
+//   var elements = document.getElementsByTagName("include");
+//   for (var i = 0; i < elements.length; i++) {
+//     var element = elements[i];
+//     var file = element.getAttribute("src");
+//     if (file) {
+//       (function(element) { // 创建一个函数作用域，以便保存每个元素的引用
+//         var xhttp = new XMLHttpRequest();
+//         xhttp.onreadystatechange = function() {
+//           if (this.readyState === 4 && this.status === 200) {
+//             element.innerHTML = this.responseText;
+//           }
+//         };
+//         xhttp.open("GET", file, true);
+//         xhttp.send();
+//       })(element);
+//     }
+//   }
+// }
+
+// document.addEventListener("DOMContentLoaded", includeHTML);
 function includeHTML() {
   var elements = document.getElementsByTagName("include");
   for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
     var file = element.getAttribute("src");
     if (file) {
-      (function(element) { // 创建一个函数作用域，以便保存每个元素的引用
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState === 4 && this.status === 200) {
-            element.innerHTML = this.responseText;
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+          element.innerHTML = this.responseText;
+          // 执行加载的JavaScript代码
+          var scripts = element.getElementsByTagName("script");
+          for (var j = 0; j < scripts.length; j++) {
+            var script = document.createElement("script");
+            script.innerHTML = scripts[j].innerHTML;
+            document.body.appendChild(script);
           }
-        };
-        xhttp.open("GET", file, true);
-        xhttp.send();
-      })(element);
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
     }
   }
 }
