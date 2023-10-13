278139b170fccc7dd582e61e76a2d634f74f7d09
PJ568 <PJ568@PJ568.eu.org>
2023-10-13 20:17:12 +0800

    优化翻译效果

--- js/loading.js
+++ js/loading.js
@@ -15,6 +15,16 @@ var time=setInterval(function () {
         //  loadingPage.remove();
         //  $('#loading').remove();
         position.style.display = 'none';
+        try{
+            translate.listener.start();
+            translate.language.setLocal('chinese_simplified');
+            translate.setAutoDiscriminateLocalLanguage();
+            translate.language.setUrlParamControl();
+        }
+        catch(e){console.log(e);}
+        translate.setUseVersion2();
+        translate.ignore.class.push('notTranslate');
+        translate.execute();
     }
 
     loadingBackground.style.opacity=opacity;
