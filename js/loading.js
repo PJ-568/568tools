871ce9ce319cbf0b6dd8c38e13c8418178ad9fe6
PJ568 <PJ568@PJ568.eu.org>
2023-10-16 09:48:22 +0800

    优化

--- js/loading.js
+++ js/loading.js
@@ -5,29 +5,29 @@ document.onreadystatechange=function () {
     }
 }
 function loadingFade() {
-var opacity=1;
-const position = document.querySelector('#loading');
-//  var loadingPage=document.getElementById('loading');
-var loadingBackground=document.getElementById('loading_bg');
-var time=setInterval(function () {
-    if (opacity<=0){
-        clearInterval(time);
-        //  loadingPage.remove();
-        //  $('#loading').remove();
-        position.style.display = 'none';
-        try{
-            translate.listener.start();
-            translate.language.setLocal('chinese_simplified');
-            translate.setAutoDiscriminateLocalLanguage();
-            translate.language.setUrlParamControl();
+    var opacity=1;
+    const position = document.querySelector('#loading');
+    //  var loadingPage=document.getElementById('loading');
+    var loadingBackground=document.getElementById('loading_bg');
+    var time=setInterval(function () {
+        if (opacity<=0){
+            clearInterval(time);
+            //  loadingPage.remove();
+            //  $('#loading').remove();
+            position.style.display = 'none';
+            try{
+                translate.listener.start();
+                translate.language.setLocal('chinese_simplified');
+                translate.setAutoDiscriminateLocalLanguage();
+                translate.language.setUrlParamControl();
+            }
+            catch(e){console.log(e);}
+            translate.setUseVersion2();
+            translate.ignore.class.push('notTranslate');
+            translate.execute();
         }
-        catch(e){console.log(e);}
-        translate.setUseVersion2();
-        translate.ignore.class.push('notTranslate');
-        translate.execute();
-    }
 
-    loadingBackground.style.opacity=opacity;
-    opacity-=0.4;
-},100);
+        loadingBackground.style.opacity=opacity;
+        opacity-=0.4;
+    },100);
 }
